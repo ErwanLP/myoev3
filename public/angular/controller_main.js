d8fc17f8e5e8acba38ed86724e65b230c022aaa6
ErwanLP <erwan.le-poder@isep.fr>
2016-01-16 12:15:08 +0100

    progress bar

--- public/angular/controller_main.js
+++ public/angular/controller_main.js
@@ -10,8 +10,16 @@ myoApp.controller('MainController', ['$scope', 'displayService', 'myoInfo', func
     console.log('Main controller init');
 
 	myoInfo.getMyoInfo(function(data){
-		var text = 'Battery = '+data.battery_level + '\n\b Bluetooth = ' + data.bluetooth_strength;
-		document.getElementById('myoInfo').innerHTML += text;
+		if(data.battery_level && data.bluetooth_strength){
+			var text = 'Battery = '+data.battery_level ;
+			document.getElementById('myoInfo_battery').innerHTML += text;
+			document.getElementById('myoInfo_battery_progressBar').value = parseInt(data.battery_level);
+
+			var text2 = 'Bluetooth = ' + data.bluetooth_strength;
+			document.getElementById('myoInfo_BT').innerHTML += text2;
+			document.getElementById('myoInfo_BT_progressBar').value = parseInt(data.bluetooth_strength);
+
+		}
 	});
 	displayService.callbackController = function (current_event) {
 		console.log('event from the client to the server : '+ current_event);
